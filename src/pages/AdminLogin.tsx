539eb5e823b00d72344866e7b85f6945813eeaa0
Naveen Maan <naveen.maan2006@gmail.com>
2026-02-27 15:08:42 +0530

    Revert "Trial"
    This reverts commit 417e60c9ead82eb16a46b629eeec0cd2229a2e10.

--- src/pages/AdminLogin.tsx
+++ src/pages/AdminLogin.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-   import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Eye, EyeOff, Lock, Mail, ShieldCheck } from 'lucide-react';
 import { saveToken } from '@/lib/auth';
 
